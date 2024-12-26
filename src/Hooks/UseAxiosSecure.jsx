b92a2cc82077de2fc6fc3e5be27c33f9a070b988
shahbaz-kamal <shahbazkamal384@gmail.com>
2024-12-26 18:12:13 +0600

    Project completed

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://suggestify-website-server.vercel.app/",
 });
 const UseAxiosSecure = () => {
   return axiosInstance;
